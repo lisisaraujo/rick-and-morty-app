1c241e4ddc81cae6554772cb6805f9472b5f8c0e
Lisis <laruschel@proton.me>
2023-02-06 10:29:17 +0100

    api for the first page fetched

--- index.js
+++ index.js
@@ -9,8 +9,34 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
 
-const hello = "hello";
+fetchDataAndRender();
+
+// --v-- your code below this line --v--
+
+async function fetchDataAndRender() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    const results = data.results;
+    console.log(results);
+    if (response.ok) {
+      results.forEach((character) => {
+        console.log(character);
+        // const newCards = Card(character);
+        // renderElement(newCards);
+        let characterArr = [results];
+        console.log(characterArr);
+      });
+
+      // return data;
+    } else {
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error("An Error occurred");
+  }
+}
